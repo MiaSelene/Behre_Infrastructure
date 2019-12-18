139a99f2ba0dbbee66893429d7bb299015036cfb
Ravid Hausmann <github@ravid.de>
2019-12-18 14:20:19 +0100

    migrate PoC webapp

---
+++ frontend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+var exphbs = require('express-handlebars');
+
+app.use('/static', express.static('public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+app.listen(3000, () => console.log('jajablabla'));
