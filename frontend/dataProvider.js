eefc124d6c339ea1493341da19c6905e23accfab
Ravid Hausmann <github@ravid.de>
2019-12-19 14:18:55 +0100

    infrastructure for event calendar

--- frontend/dataProvider.js
+++ frontend/dataProvider.js
@@ -1,1 +1,12 @@
-localStorage["bar"] = "foo";
+function SetInterests(interests){
+  for (var i=0; i<interests.length; i++) {
+    localStorage[i] = true;
+  }
+}
+//Event Types 1: Party; 2: Art; 3: Library; 4: Group activity
+function InterestedIn(EventID){
+  if(localStorage[EventID]){
+    return true;
+  }
+  return false;
+}
