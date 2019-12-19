9126beb9467eeefdb6da0b6b25fcafe47596e8d9
Ravid Hausmann <github@ravid.de>
2019-12-19 17:28:04 +0100

    add pictures to settings screen

--- frontend/public/dataProvider.js
+++ frontend/public/dataProvider.js
@@ -1,9 +1,11 @@
-function SetInterests(interests){
-  for (var i=0; i<interests.length; i++) {
-    localStorage[i] = true;
+
+function SetInterest(interest){
+  if (localStorage[interest]){
+    localStorage[interest] = false;
+  }else{
+    localStorage[interest] = true;
   }
 }
-console.log('exist..')
 //Event Types 1: Party; 2: Art; 3: Library; 4: Group activity
 function InterestedIn(EventID){
   if(localStorage[EventID]){
