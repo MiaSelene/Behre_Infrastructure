0fd20246a5a9059356a2438b7143530569c9ba92
Ravid Hausmann <github@ravid.de>
2020-01-10 16:29:38 +0100

    Add Event inspection animation

--- frontend/public/home.js
+++ frontend/public/home.js
@@ -1,4 +1,5 @@
 var cal = GetEvents()
+changeScore(0);
 for (var i=0; i<cal.length; i++){
   var Event = ["party","reading","art","group"][parseInt(cal[i].ID)-1];
   var content = cal[i]
@@ -7,20 +8,26 @@ for (var i=0; i<cal.length; i++){
 
   }
 }
+document.getElementById("recommendedevent").innerHTML=content["name"];
+
 function changeScore(i){
+  if (!("Score" in localStorage) ){localStorage["Score"]=100;}
   var score = document.getElementById('pointscore');
   score.innerHTML = i + parseInt(localStorage["Score"]);
   localStorage["Score"]=score.innerHTML;
 }
+
+
 function changeEventVisibility(){
-  if(document.getElementById("Event").style.display=="block"){
-    document.getElementById("Event").style.display="none";
+  if(document.getElementById("Event").classList.contains("anim")){
+      document.getElementById("Event").classList.remove("anim");
   }else{
     document.getElementById("Event").innerHTML=document.getElementById("recommendedevent").innerHTML;
-    document.getElementById("Event").style.display="block";
+    document.getElementById("Event").classList.add("anim");
   }
-
 }
+
+
 function CreateCell(Event, name, people, price, date){
   var table = document.getElementById("calendar");
   var row = table.insertRow(0);
@@ -34,4 +41,6 @@ function CreateCell(Event, name, people, price, date){
 
   })
 }
-changeScore(0);
+function buy(){
+  localStorage["Score"] = parseInt(localStorage["Score"]) -10;
+}
